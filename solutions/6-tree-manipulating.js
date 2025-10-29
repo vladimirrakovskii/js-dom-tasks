0c958a92b096db146f485e7d858b4222d6e070e3
vladimirrakovskii <v.rakovscky@yandex.ru>
2025-10-29 16:33:15 +0300

    Update 6-tree-manipulating.js

--- solutions/6-tree-manipulating.js
+++ solutions/6-tree-manipulating.js
@@ -1,3 +1,13 @@
 // BEGIN
-
+export default () => {
+    document.querySelectorAll('div').forEach(div => {
+    [...div.childNodes].forEach(child => {
+      if (child.nodeType === Node.TEXT_NODE && child.textContent.trim() !== '') {
+        const p = document.createElement('p');
+        p.textContent = child.textContent;
+        div.replaceChild(p, child);
+      }
+    });
+  });
+}
 // END
