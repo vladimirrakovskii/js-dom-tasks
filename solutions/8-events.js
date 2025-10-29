71de7a96d78f0e6d22fb19b9ed2a5f99ff0f5033
vladimirrakovskii <v.rakovscky@yandex.ru>
2025-10-29 16:34:43 +0300

    Update 8-events.js

--- solutions/8-events.js
+++ solutions/8-events.js
@@ -1,5 +1,13 @@
 export default () => {
   // BEGIN
-
+const btn = document.getElementById('alert-generator');
+  const container = document.querySelector('.alerts');
+  let count = 0;
+  btn?.addEventListener('click', () => {
+    const alert = document.createElement('div');
+    alert.className = 'alert alert-primary';
+    alert.textContent = `Alert ${++count}`;
+    container.insertBefore(alert, container.firstChild);
+  });
   // END
 };
