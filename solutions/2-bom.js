733116f2ace20f322ea3a93ce24f77317d811e25
vladimirrakovskii <v.rakovscky@yandex.ru>
2025-10-29 16:27:02 +0300

    Update 2-bom.js

--- solutions/2-bom.js
+++ solutions/2-bom.js
@@ -1,3 +1,6 @@
 // BEGIN
-
+export default (targetUrl) => {
+    window.location.href = targetUrl;
+    return navigator.appCodeName + '/' + navigator.appVersion + ' ' + window.location.href;
+}
 // END
