c6f64c70767ecd55509b980d32c6bc16530aa15d
vladimirrakovskii <v.rakovscky@yandex.ru>
2025-10-29 16:32:35 +0300

    Update 5-search.js

--- solutions/5-search.js
+++ solutions/5-search.js
@@ -1,5 +1,14 @@
 export default (document) => {
   // BEGIN
-
+const titleEl = document.querySelector('h1');
+  const descEl = document.querySelector('.description');
+  return {
+    title: titleEl?.textContent ?? '',
+    description: descEl?.textContent ?? '',
+    items: [...document.querySelectorAll('.links > div')].map(div => ({
+      title: div.querySelector('h2 a')?.textContent ?? '',
+      description: div.querySelector('p')?.textContent ?? '',
+    })),
+  };
   // END
 };
