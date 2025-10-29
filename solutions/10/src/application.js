516c2ea1abfe2a79308b32f6556712ba93454489
vladimirrakovskii <v.rakovscky@yandex.ru>
2025-10-29 16:38:47 +0300

    Update application.js

--- solutions/10/src/application.js
+++ solutions/10/src/application.js
@@ -1,5 +1,25 @@
 import escapeHtml from 'escape-html';
 
 // BEGIN
+export default () => {
+  const form = document.querySelector('form');
 
+  form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const email = form.elements.email.value;
+    const name = form.elements.name.value;
+    const comment = form.elements.comment.value;
+    const escapedEmail = escapeHtml(email);
+    const escapedName = escapeHtml(name);
+    const escapedComment = escapeHtml(comment);
+    const feedbackDiv = document.createElement('div');
+    feedbackDiv.innerHTML = 
+      `<p>Feedback has been sent</p>
+      <div>Email: ${escapedEmail}</div>
+      <div>Name: ${escapedName}</div>
+      <div>Comment: ${escapedComment}</div>`;
+
+    form.replaceWith(feedbackDiv);
+  });
+};
 // END
