03ac8d2bdd69b0bfc446a81addda609a2de333e8
vladimirrakovskii <v.rakovscky@yandex.ru>
2025-10-29 16:34:17 +0300

    Update 7-element-manipulating.js

--- solutions/7-element-manipulating.js
+++ solutions/7-element-manipulating.js
@@ -1,5 +1,14 @@
 import camelCase from 'lodash/camelCase';
 
 // BEGIN
-
+export default (doc) => {
+  [...doc.body.querySelectorAll('*')].forEach(el => {
+    [...el.classList].forEach(cls => {
+      const camel = camelCase(cls);
+      if (camel !== cls) {
+        el.classList.replace(cls, camel);
+      }
+    });
+  });
+};
 // END
