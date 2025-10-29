938a822a11ecaa035d93d437931a58c675876513
vladimirrakovskii <v.rakovscky@yandex.ru>
2025-10-29 16:28:09 +0300

    Update 4-dom-hierarchy.js

--- solutions/4-dom-hierarchy.js
+++ solutions/4-dom-hierarchy.js
@@ -1,3 +1,12 @@
 // BEGIN
-
+export default (el) => {
+  const txts = [];
+  const findP = (node) => {
+    if (node.nodeType === 1) {
+      node.tagName === 'P' ? txts.push(node.textContent.trim()) : [...node.children].forEach(findP);
+    }
+  };
+  findP(el);
+  return txts;
+};
 // END
